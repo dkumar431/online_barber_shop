fc6807a0fd88de79866b93ccdf39a453cf226ec5
Deepak <dkumar431@gmail.com>
2017-01-14 18:46:28 +0530

    Added unit tests for model validations.

--- spec/models/booking_spec.rb
+++ spec/models/booking_spec.rb
@@ -15,26 +15,44 @@ RSpec.describe Booking, type: :model do
 	end
 
   describe 'validation' do
+    let(:booking) { Booking.new(slot_id: 5, booking_date: Date.today) }
+    let(:customer) {
+      Customer.new(name: 'Johnson', phone: '282-393-4945',email: 'johnson@gmail.com', address: 'LOC', age: 26)
+    }
+    before(:each) do
+      booking.customer = customer
+    end
     context '#slot_id'  do
       it 'object is valid if slot_id present' do
+        expect(booking).to be_valid
       end
 
       it 'object is invalid if slot_id is blank ' do
+        booking.slot_id = nil
+        expect(booking).not_to be_valid
+        expect(booking).to have(1).errors_on(:slot_id)
       end
     end
     context '#booking_date' do
       it 'object is valid if booking_date is present' do
+        expect(booking).to be_valid
       end
       it 'object is invalid if booking_date is empty' do
+        booking.booking_date = nil
+        expect(booking).not_to be_valid
+        expect(booking).to have(1).errors_on(:booking_date)
       end
     end
     context '#customer' do
+      it 'object is valid if customer is present' do
+        booking.customer = customer
+        expect(booking).to be_valid
+      end
+      it 'object is invalid if customer is absent' do
+        booking.customer = nil
+        expect(booking).not_to be_valid
+        expect(booking).to have(1).errors_on(:customer)
+      end
     end
   end
 end
-
-=begin
-validates :slot_id, presence: true
-  validates :booking_date, presence: true
-  validates_presence_of :customer
-=end
