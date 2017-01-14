b6a7c772a61bb39417dfbf46ccdcbe8d61bd7062
Deepak <dkumar431@gmail.com>
2017-01-14 18:46:42 +0530

    seeded the project

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,40 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+Slot.destroy_all
+Customer.destroy_all 
 
 (10..18).each do |time|
   status = time == 14 ? 0 : 1
-  Slot.create({start_time: time, end_time: time+1, status: status})
+  Slot.create(start_time: time, end_time: time+1, status: status)
 end
 
+slots = Slot.all.where(status: true).pluck(:id)
+dates =  (0..10).map {|x| (Date.today + x).strftime("%Y-%m-%d") }
+
+dates.each do |booking_date|
+  slots.sample(4).each do  |slot_id|
+    booking = Booking.new(slot_id: slot_id, booking_date: booking_date)
+    booking.customer = Customer.new({
+      name: Faker::Name.name,
+      email: Faker::Internet.email,
+      phone: Faker::PhoneNumber.phone_number,
+      address: Faker::Address.street_name 
+    })    
+    booking.save!
+  end
+end
+
+puts 'Seeded DB with slots, bookings & customers.'
+=begin
+(0..15).each do
+  customer = Customer.new({
+    name: Faker::Name.name,
+    email: Faker::Internet.email,
+    phone: Faker::PhoneNumber.phone_number,
+    address: Faker::Address.street_name })
+  booking = Booking.new({slot_id: slots.sample,booking_date: dates.sample})
+  booking.customer = customer
+  booking.save
+end
+=end
